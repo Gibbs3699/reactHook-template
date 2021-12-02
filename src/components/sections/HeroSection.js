d82cde1689b7e0ff4e700a18a27b6db22d16a9e9
Gibbs3699 <gift_kamonpan@hotmail.com>
2021-12-02 23:30:19 +0700

    Adding text/color styles

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,26 +1,43 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import styled from "styled-components"
+import { themes } from "../styles/ColorStyles"
+import {H1, MediumText} from "../styles/TextStyles"
 
 function HeroSection() {
-    return (
-        <Wrapper>
-            <ContentWrapper>
-                <img src="/images/logos/logo.svg" alt="logo" />
-                <h1>Design and code SwiftUI apps</h1>
-                <p>Welcome to your new Gatsby site.</p>
-                <Link to="/page-2/">Go to page 2</Link> <br />
-            </ContentWrapper>
-        </Wrapper>
-    )
+  return (
+    <Wrapper>
+      <ContentWrapper>
+        <TextWrapper>
+          <Title>
+            Design <br />
+            and code SwiftUI apps
+          </Title>
+          <Description>Welcome to your new Gatsby site.</Description>
+        </TextWrapper>
+      </ContentWrapper>
+    </Wrapper>
+  )
 }
 
 export default HeroSection
 
 // choose div as html tag
 const Wrapper = styled.div`
-    background: linear-gradient(180deg, #4316DB 0%, #9076E7 100%);
+  background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
 `
 const ContentWrapper = styled.div`
-    max-width: 1234px;
+  max-width: 1234px;
+  margin: 0 auto;
+  padding: 200px 60px;
+`
+const TextWrapper = styled.div`
+  max-width: 360px;
+  display: grid;
+  gap: 30px;
+`
+const Title = styled(H1)`
+  color: ${themes.light.text1};
+`
+const Description = styled(MediumText)`
 `
---
+++ src/components/styles/ColorStyles.js
@@ -0,0 +1,12 @@
+export const themes = {
+    light: {
+        text1: "black",
+        text2: "rgb(0,0,0,0.7)",
+        backgroundColor: "#F2F6FF"
+    },
+    dark: {
+        text1: "white",
+        text2: "rgb(255,255,255,0.7)",
+        backgroundColor: "#1F1F47"
+    }
+}
